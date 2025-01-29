63697eae037fab784d5559c5172ceca4557ab4e9
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-30 00:03:40 +0530

    recent updates

---
+++ app/src/main/java/com/saif/mywhatsapp/MyApp.java
@@ -0,0 +1,36 @@
+package com.saif.mywhatsapp;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance()
+        );
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "whatsapp_notification_channel",
+                    "WhatsApp Notifications",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Default notification channel for WhatsApp clone");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
