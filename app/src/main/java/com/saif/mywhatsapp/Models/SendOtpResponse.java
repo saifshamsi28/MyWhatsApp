57cb2817c8fbce9906eae72b7cf8f665b462d408
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-31 07:45:32 +0530

    created class for Supabase and twilio Phone Auth

---
+++ app/src/main/java/com/saif/mywhatsapp/Models/SendOtpResponse.java
@@ -0,0 +1,13 @@
+package com.saif.mywhatsapp.Models;
+
+public class SendOtpResponse {
+    String otp_sent; // or any relevant fields you expect in the response
+
+    public String getOtp_sent() {
+        return otp_sent;
+    }
+
+    public void setOtp_sent(String otp_sent) {
+        this.otp_sent = otp_sent;
+    }
+}
---
+++ app/src/main/java/com/saif/mywhatsapp/SupabaseClient.java
@@ -0,0 +1,50 @@
+package com.saif.mywhatsapp;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SupabaseClient {
+
+    private static final String BASE_URL = "https://issnpiizowpkyzkuselm.supabase.co"; // Replace with your actual base URL
+    private static final String API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlzc25waWl6b3dwa3l6a3VzZWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMDQ1MzMsImV4cCI6MjA1MzY4MDUzM30.pFEpj0E_NIqcmd0UIQXuOUVLAQoW8j_c-Gd56XPYoZo"; // Replace with your actual API key
+//    const phoneNumber = "+15551234567"; // Example phone number
+//    const url = `https://your-supabase-project-url/auth/v1/phone/sign_in_with_otp?phone=${phoneNumber}`;
+
+    // Make an API call to this URL to send the OTP
+
+    private Retrofit retrofit;
+
+
+    public SupabaseAuthApi getAuthApi() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("apikey", API_KEY) // Add the API key in the header
+                                    .addHeader("Content-Type", "application/json")
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    }).build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(SupabaseAuthApi.class);
+    }
+}
+//curl -X POST 'https://cvwawazfelidkloqmbma.supabase.co/auth/v1/otp' \
+//        -H "apikey: SUPABASE_KEY" \
+//        -H "Content-Type: application/json" \
+//        -d '{
+//        "phone": "+13334445555"
+//        }'
